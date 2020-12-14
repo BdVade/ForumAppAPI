fb1313e344b87b6ac21df515331359cfec62ec34
BdVade <victoraderibigbe03@gmail.com>
2020-12-14 16:46:48 +0100

    Update settings.py

--- ForumApi/settings.py
+++ ForumApi/settings.py
@@ -84,7 +84,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+    'DEFAULT_METADATA_CLASS' : 'rest_framework.metadata.SimpleMetadata'
 }
 
 
